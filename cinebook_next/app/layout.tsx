a1c618ea9019a025a01fe2da8405c42b4f4785bc
SabinShrestha133 <stha.sabin133@gmail.com>
2026-07-28 23:07:00 +0545

    refactor layout.tsx to wrap AuthProvider with ThemeProvider and set data-theme attribute

--- cinebook_next/app/layout.tsx
+++ cinebook_next/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "@/lib/contexts/AuthContext";
+import { ThemeProvider } from "@/lib/contexts/ThemeContext";
 
 export const metadata: Metadata = {
   title: "CineBook",
@@ -25,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" data-theme="dark" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        <AuthProvider>
-          {children}
-          <ToastContainer position="top-right" autoClose={3000} />
-        </AuthProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            {children}
+            <ToastContainer position="top-right" autoClose={3000} />
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
